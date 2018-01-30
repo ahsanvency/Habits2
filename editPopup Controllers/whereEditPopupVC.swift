d4180ad5e18c5636b86971510c5396666247b9f6
Sai Kasam <saikasam98@gmail.com>
2018-01-30 09:27:53 -0600

    pushing where update to firebase

--- editPopup Controllers/whereEditPopupVC.swift
+++ editPopup Controllers/whereEditPopupVC.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import Firebase
 
 class whereEditPopupVC: UIViewController {
-
+    @IBOutlet weak var whereText: fancyField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,8 +19,36 @@ class whereEditPopupVC: UIViewController {
     }
     
     @IBAction func saveButton(_ sender: Any) {
-        dismiss(animated: true) {
+        if whereText.text != ""{
+            
+            var ref: DatabaseReference!
+            ref = Database.database().reference()
+            
+            //current user
+            guard let user = Auth.auth().currentUser else {
+                return
+            }
+            let uid = user.uid
+            
+            ref.child("Habits").child(uid).observeSingleEvent(of: .value, with: { (snapshot) in
+                // Get user value
+                let value = snapshot.value as? NSDictionary
+                guard let firstKey = value?.allKeys[0] else {
+                    print("n")
+                    return }
+                ref.child("Habits").child(uid).child("\(firstKey)").updateChildValues(["Where":self.whereText.text])
+            }) { (error) in
+                print(error.localizedDescription)
+            }
+            
+                let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                let whenView = storyBoard.instantiateViewController(withIdentifier: "MainScreenViewCID") as! MainScreenViewC
+                
+                self.present(whenView,animated: true, completion: nil)
             
         }
+        
+        
+        
     }
 }
